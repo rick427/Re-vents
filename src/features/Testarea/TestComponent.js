731bf4029c8f4b048317f62e5d92b6015a57a545
rick427 <44168932+rick427@users.noreply.github.com>
2019-06-09 07:35:32 +0100

    finishing up redux test env and adding dev tools

--- src/features/Testarea/TestComponent.js
+++ src/features/Testarea/TestComponent.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {incrementCounter, decrementCounter} from './testAction';
+import { Button } from 'semantic-ui-react';
+
+const mapState = (state) => ({
+    data: state.test.data
+});
+
+const actions = {
+    incrementCounter,
+    decrementCounter
+}
 
 class TestComponent extends Component {
     render() {
+        const {incrementCounter, decrementCounter, data} = this.props;
         return (
             <div>
                 <h1>Test Component</h1>
+                <h3>The answer is: {data} </h3>
+                <Button onClick={incrementCounter} positive content="increment"/>
+                <Button onClick={decrementCounter} negative content="decrement"/>
             </div>
         )
     }
 }
 
-export default TestComponent;
+export default connect(mapState, actions)(TestComponent);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/layout/App';
+import {Provider} from 'react-redux';
+import {configureStore} from './app/store/configureStore';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+const store = configureStore();
+console.log(store.getState())
+
 ReactDOM.render(
+  <Provider store={store}>
   <Router>
     <App />
   </Router>
+  </Provider>
     ,document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
