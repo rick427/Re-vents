8feeb4a539d891ee47d8873d9fc83e798246a2d3
rick427 <44168932+rick427@users.noreply.github.com>
2019-06-09 19:59:13 +0100

    fixing scrolling to the top issue on change route

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import {Provider} from 'react-redux';
 import {configureStore} from './app/store/configureStore';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './app/common/util/ScrollToTop';
 
 const store = configureStore();
 console.log(store.getState())
 
 ReactDOM.render(
   <Provider store={store}>
-  <Router>
-    <App />
-  </Router>
+    <Router>
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
+    </Router>
   </Provider>
     ,document.getElementById('root'));
 
