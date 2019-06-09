affc4e39091aff1ed19de948a7e08bf23e6d9c27
rick427 <44168932+rick427@users.noreply.github.com>
2019-06-09 14:41:00 +0100

    adjusting rdr switch with a reducer util function

--- src/features/Testarea/testReducer.js
+++ src/features/Testarea/testReducer.js
@@ -1,24 +1,37 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "./testTypes";
+import { createReducer } from "../../app/common/util/reducerUtils";
 
 const initialState = {
     data: 42
+};
+
+const incrementCounter = (state) => {
+    return {...state, data: state.data + 1};
 }
 
-const testReducer = (state = initialState, action) => {
-    switch (action.type){
-        case INCREMENT_COUNTER:
-            return {
-                ...state,
-                data: state.data + 1
-            };
-        case DECREMENT_COUNTER:
-            return {
-                ...state,
-                data: state.data - 1
-            };
-        default:
-            return state;
-    }
+const decrementCounter = (state) => {
+    return {...state, data: state.data - 1};
 }
 
-export default testReducer;
+export default createReducer(initialState, {
+    [INCREMENT_COUNTER]: incrementCounter,
+    [DECREMENT_COUNTER]: decrementCounter
+});
+
+
+// const testReducer = (state = initialState, action) => {
+//     switch (action.type){
+//         case INCREMENT_COUNTER:
+//             return {
+//                 ...state,
+//                 data: state.data + 1
+//             };
+//         case DECREMENT_COUNTER:
+//             return {
+//                 ...state,
+//                 data: state.data - 1
+//             };
+//         default:
+//             return state;
+//     }
+// }
